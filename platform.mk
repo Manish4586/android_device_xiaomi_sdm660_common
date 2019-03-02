f8ecd0b45a086d116f66f959cf446945140ac5bb
Manish4586 <manish.n.manish45@gmail.com>
2019-03-02 18:29:28 +0530

    adapt to caf

--- platform.mk
+++ platform.mk
@@ -53,11 +53,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	AntHalService \
 	antradio_app \
-	com.dsi.ant.antradio_library \
 	libantradio
-
-PRODUCT_COPY_FILES += \
-	external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Audio
 PRODUCT_PACKAGES += \
@@ -94,9 +90,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.vc_call_vol_steps=4 \
 	ro.config.alarm_vol_steps=6 \
 	ro.config.ring_vol_steps=7
-	
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    sys.display-size=3840x2160
 	
 PRODUCT_CHARACTERISTICS := nosdcard	
 
@@ -391,13 +384,6 @@ PRODUCT_PACKAGES += \
 # Sensor HAL conf file
 PRODUCT_COPY_FILES += \
 	$(PLATFORM_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
-# Telephony
-PRODUCT_PACKAGES += \
-	telephony-ext
-
-PRODUCT_BOOT_JARS += \
-	telephony-ext
 
 # TextClassifier
 PRODUCT_PACKAGES += \
