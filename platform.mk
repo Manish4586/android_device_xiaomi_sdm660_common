e9a5dbc998c27c40ee89a697f56f931a6c57f13c
Manish4586 <manish.n.manish45@gmail.com>
2019-03-20 02:32:15 +0530

    sdm660-common: media vol 15 steps

--- platform.mk
+++ platform.mk
@@ -88,7 +88,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.af.client_heap_size_kbyte=7168 \
-	ro.config.media_vol_steps=25 \
+	ro.config.media_vol_steps=15 \
 	ro.config.vc_call_vol_steps=7
 
 # Bluetooth
