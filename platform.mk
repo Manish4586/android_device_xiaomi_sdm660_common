1988b0ce5bf6bb8515a7c7e1675a3f56e9c56379
Manish4586 <manish.n.manish45@gmail.com>
2019-03-21 02:50:57 +0530

    sdm660: set to xxhdpi

--- platform.mk
+++ platform.mk
@@ -49,6 +49,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
 	fs_config_files
 
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi	
+	
 # ANT+
 PRODUCT_PACKAGES += \
 	AntHalService \
