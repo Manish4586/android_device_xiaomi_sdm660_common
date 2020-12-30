c1d1124bc97b4cbc15e952df4b26dcd815905ca3
Manish4586 <manish.n.manish45@gmail.com>
2020-12-30 08:54:08 +0530

    sdm660-common: remove derprecated target provide qti jar
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>
    Change-Id: I926aa00b8a628ed1e779d7c8fc59e0c96e828bca

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -221,7 +221,6 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
 # RIL
 PROTOBUF_SUPPORTED := true
-TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # SELinux
 ifneq ($(wildcard device/qcom/sepolicy-legacy-um/SEPolicy.mk),)
