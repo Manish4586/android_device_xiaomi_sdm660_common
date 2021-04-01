9b5ca08aa12102b7fa73f2db8cb9ded6db57b6b5
OdSazib <odsazib@gmail.com>
2021-04-01 22:04:33 +0530

    sdm660-common: Build power stats and label it
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -436,7 +436,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr
+    android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
